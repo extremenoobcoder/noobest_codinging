3c0915884c0ff15afb17d7ad37be74f5fdc00409
extremenoobcoder <77008509+extremenoobcoder@users.noreply.github.com>
2021-01-07 08:35:25 +0400

    Add files via upload

---
+++ Noobcoding.py
@@ -0,0 +1,3 @@
+aye computera say what noob coders say print ("Hellos, worlds")
+if(you not print hello world i call Samuel Miller and he make your pp that small that we can't even see')
+    else(you f off)
